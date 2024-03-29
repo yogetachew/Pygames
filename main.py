aaf8a38fd704213d994c1a9607ebc9cf93b052db
yogetachew <126614232+yogetachew@users.noreply.github.com>
2024-03-29 01:53:39 -0600

    Stars, levels, and lives

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time 
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 650, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -23,15 +24,31 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # load the backgound 
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 def main():
     run = True
     FPS = 60
-    clock = pygame.time.Clock
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
     
+    clock = pygame.time.Clock()
+    
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+        WIN.blit(lives_label, (10,10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        
+        pygame.display.update()
+        
     while run:
         clock.tick(FPS)
+        redraw_window()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
